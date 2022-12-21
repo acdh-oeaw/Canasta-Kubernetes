5a7ce8db0b902eff29089c5d350f0693e8e5d7b0
Dalibor Pančić <dalibor.pancic@gmail.com>
2022-12-21 10:16:45 +0100

    add fina namespace

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
 # Delete previous ConfigMaps
-kubectl delete configmap canasta-settings
-kubectl delete configmap canasta-config
-kubectl delete configmap canasta-env
+kubectl -n fina delete configmap canasta-settings
+kubectl -n fina delete configmap canasta-config
+kubectl -n fina delete configmap canasta-env
 
 # Create ConfigMaps from the configuration files
-kubectl create configmap canasta-settings --from-file=./settings/
-kubectl create configmap canasta-config --from-file=./config/
-kubectl create configmap canasta-env --from-env-file=.env
+kubectl -n fina create configmap canasta-settings --from-file=./settings/
+kubectl -n fina create configmap canasta-config --from-file=./config/
+kubectl -n fina create configmap canasta-env --from-env-file=.env
 
 # Create all the Kubernetes deployments, services and related objects
-kubectl apply -f Kubernetes/
+kubectl -n fina apply -f Kubernetes/
